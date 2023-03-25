b7282c3c9ca669bfabbd65ea785bbb4c6a0fb4d2
Alexsey <alex_frieza@mail.ru>
2023-03-25 18:52:54 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
