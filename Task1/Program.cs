1af683651f3f736a5a70b22cb89b155ee3089284
Alexsey <alex_frieza@mail.ru>
2023-03-25 18:50:02 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите номер А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+int max = numA;
+int min = numA;
+
+if (numA < numB)
+{
+    min = numA;
+    max = numB;
+    Console.WriteLine($"max = {max} \nmin = {min}");
+}
+else if (numB < numA)
+{
+    max = numA;
+    min = numB;
+    Console.WriteLine($"max = {max} \nmin = {min}");
+}
+
+else
+{
+    Console.WriteLine("Числа равны");
+}
