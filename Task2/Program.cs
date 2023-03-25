a2ad298169ea5bc93d0796c10dcd3bea36b77548
Alexsey <alex_frieza@mail.ru>
2023-03-25 18:51:47 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите номер А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер B");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер C");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (numA < numB)
+{
+    max = numB;
+}
+if (max < numC)
+{
+    max = numC;
+}
+
+Console.WriteLine($"max = {max}");
