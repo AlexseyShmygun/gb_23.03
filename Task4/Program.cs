e3009bc67df0cfebac0030340c8a5bfb7f1bcd25
Alexsey <alex_frieza@mail.ru>
2023-03-25 18:54:21 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 2;
+
+if (number > 1)
+{
+    while (index <= number)
+    {
+        Console.Write(index + " ");
+        index = index + 2;
+    }
+}
