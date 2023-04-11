6e54f3c648c00539cd7fe3d3558a6583d316b727
Alexsey <alex_frieza@mail.ru>
2023-04-11 21:55:44 +0300

    Add Task1 Folder Less4 and Update file Dotnet all folder

--- Less 4/Task1/Program.cs
+++ Less 4/Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] numbers = new int[5];
+Random rand = new();
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rand.Next(100, 1000);
+
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(string.Join(", ", numbers));
+Console.WriteLine($"Колличество четных чисел в массиве = {count}");
